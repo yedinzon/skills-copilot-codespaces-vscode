bd6eb511d6c2dfc1b0342b8a17ca451a0e122065
yedinzon <yedinzon@gmail.com>
2023-11-06 21:24:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'}); // set the response header
+  res.write('Hello World'); // write the response body
+  res.end(); // end the response
+});
+
+server.listen(5000, function() {
+  console.log('Node.js web server at port 5000 is running..');
+});
